f2f928651dc0f0607e646c11ab42ec478726635a
DrTCortes <thiagocortes@outlook.com.br>
2021-11-19 02:12:41 -0300

    Criando e consultando vendas

--- services/pagarme.js
+++ services/pagarme.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const instanceAxios = axios.create({
     baseURL: "https://api.pagar.me/1/",
     params:{
-        api_key: "ak_test_qRjc2viEfQsjrfsEGj9jqZjdL3lv1O"
+        api_key: "*****************"
     }
 });
 
